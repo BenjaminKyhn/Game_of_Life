f2eb47f2c76b31b03b5ec3d6d4c0a54f23fbbd5b
Benjamin Kyhn <benkyhn@hotmail.com>
2020-04-03 12:37:08 +0200

    Cleaned up Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-
-import java.awt.*;
 
 public class Main extends Application {
     @Override
