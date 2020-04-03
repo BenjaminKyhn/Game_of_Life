4c896543f0cb085a0ecefe1b70e82291575a4014
Benjamin Kyhn <benkyhn@hotmail.com>
2020-04-03 12:16:51 +0200

    Main shows a GUI with an empty grid

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class Main extends Application {
     @Override
@@ -11,6 +19,19 @@ public class Main extends Application {
         game.clearBoard(board);
         game.tetrominoPattern1(board, 10, 10);
         game.printBoard(board);
+
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+        Rectangle[][] squares = new Rectangle[game.getBoardSize()][game.getBoardSize()];
+        for (int i = 0; i < game.getBoardSize(); i++)
+            for (int j = 0; j < game.getBoardSize(); j++) {
+                gridPane.add(squares[i][j] = new Rectangle(25, 25, Color.WHITE), j, i);
+                squares[i][j].setStroke(Color.BLACK);
+            }
+
+        Scene scene = new Scene(gridPane, 25 * game.getBoardSize() + 50, 25 * game.getBoardSize() + 50);
+        stage.setScene(scene);
+        stage.show();
 
         do {
             // Make a copy of the board
